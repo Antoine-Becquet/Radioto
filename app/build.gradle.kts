fbe7bb0c94784a834ec3700233162964e969c546
antoine <antoinebecquet93@gmail.com>
2026-02-19 17:28:13 +0100

    1.0 > 2.0 !

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.antoinebecquet.radioto"
         minSdk = 28
         targetSdk = 34 // Il est bon de garder targetSdk = compileSdk
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
